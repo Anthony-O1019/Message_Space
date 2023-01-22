6cca3dc12bf7b37acf548baa9e4452a7baa048cc
Anthony Oliveraz <105612877+Anthony-O1019@users.noreply.github.com>
2023-01-22 01:26:13 -0600

    Successfully connected to sql server

--- Message-Space/server.py
+++ Message-Space/server.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template  # Import Flask to allow us to create our app
+from user import User
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
@@ -6,6 +7,8 @@ def hello_world():
 
 @app.route('/login')
 def login():
-    return render_template('index.html')
+    users = User.get_all()
+    print (users)
+    return render_template('index.html', users =users)
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
