b588e3e99591d824fb7bb4d88c8c90f62a6437bb
Anthony Oliveraz <105612877+Anthony-O1019@users.noreply.github.com>
2023-01-31 22:07:38 -0600

    updated the user profile page

--- Message-Space/server.py
+++ Message-Space/server.py
@@ -1,14 +1,16 @@
 from flask import Flask,render_template  # Import Flask to allow us to create our app
 from user import User
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
 
-@app.route('/login')
+@app.route('/login')    #  The "@" decorator associates this route with the function immediately following
 def login():
     users = User.get_all()
     print (users)
-    return render_template('index.html', users =users)
+    return render_template('index.html', users =users)  # renders the html of the index.html file
+
+@app.route('/profile_page')
+def home():
+    return render_template('user_profile_page.html')
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
